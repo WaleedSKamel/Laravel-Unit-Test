1dadb9895a730877aaef89c16d5f27e52857adec
Waleed Said Kamel <waleedsaid014@gmail.com>
2021-02-23 16:57:55 +0200

    updated in model book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,10 +9,47 @@ class Book extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'author'];
+    protected $fillable = ['title', 'author_id'];
 
     public function path()
     {
         return '/books/'.$this->id;
     }
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = Author::firstOrCreate([
+            'name' => $author
+        ])->id;
+    }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+            'book_id' => $this->id,
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservations = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+        if (is_null($reservations)){
+            throw new \Exception();
+        }
+        $reservations->update([
+           'checked_in_at' => now()
+        ]);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+
 }
