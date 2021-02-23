87fd0fe4ba2d56f838d6356dd8b29a94b3e2594e
Waleed Said Kamel <waleedsaid014@gmail.com>
2021-02-23 09:52:55 +0200

    wip for books resource

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -7,23 +7,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
     /** @test */
     public function a_book_can_be_to_the_library()
     {
         // if you want real error
-        $this->withoutExceptionHandling();
+        //$this->withoutExceptionHandling();
 
         $response = $this->post('/books',[
             'title' => 'Book',
             'author' => 'Waleed'
         ]);
 
-        $response->assertOk();
+        $books = Book::first();
 
-       $this->assertCount(1,Book::all());
+        //$response->assertOk();
+        $this->assertCount(1,Book::all());
+        $response->assertRedirect($books->path());
     }
 
     /** @test*/
@@ -59,12 +61,33 @@ class BookReservationTest extends TestCase
 
         $books = Book::first();
 
-        $response = $this->patch('/books/'.$books->id,[
+        $response = $this->patch($books->path(),[
             'title' => 'New Title',
             'author' => 'New Author'
         ]);
 
         $this->assertEquals('New Title',Book::first()->title);
         $this->assertEquals('New Author',Book::first()->author);
+
+        $response->assertRedirect($books->fresh()->path());
+    }
+
+    /** @test*/
+    public function a_book_can_deleted()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books',[
+            'title' => 'Col Title',
+            'author' => 'Waleed'
+        ]);
+
+
+        $books = Book::first();
+        $this->assertCount(1,Book::all());
+
+        $response = $this->delete($books->path());
+
+        $this->assertCount(0,Book::all());
+        $response->assertRedirect('/books');
     }
 }
